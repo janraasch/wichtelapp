e446d364c3c99261faba9c0066dc6042ca7a6c05
Jan Raasch <jan@janraasch.com>
2025-12-04 15:59:56 +0100

    refactor: rename `wishlist` url to `home` & `_`-prefix private view functions

--- wichtel/views.py
+++ wichtel/views.py
@@ -11,23 +11,21 @@ from .models import Event, UserDrawing, Wishlist
 def home(request):
     current_year = datetime.now().year
     user_drawing = UserDrawing.objects.filter(giver=request.user, drawing__year=current_year).first()
-    if user_drawing and user_drawing.receiver.wishlist and user_drawing.receiver.wishlist.text:
-        return giftlist(user_drawing.receiver.wishlist, request)
-    return wishlist(request)
+    if user_drawing:
+        return _giftlist(user_drawing.receiver.wishlist, request)
+    return _wishlist(request)
 
-
-def wishlist(request):
-    wishlist, created = Wishlist.objects.get_or_create(user=request.user, defaults={"text": ""})
+def _wishlist(request):
+    wishlist = Wishlist.objects.get_or_create(user=request.user, defaults={"text": ""})[0]
     if request.method == "POST":
         wishlist.text = request.POST.get("text", "")
         wishlist.save()
         Event.objects.create(user=request.user, name="wishlist.saved")
         messages.success(request, "Wunschliste gespeichert!")
-        return redirect("wichtel:wishlist")
+        return redirect("wichtel:home")
     Event.objects.create(user=request.user, name="wishlist.viewed")
     return render(request, "wichtel/wishlist.html", {"wishlist": wishlist})
 
-
-def giftlist(giftlist, request):
+def _giftlist(giftlist, request):
     address = giftlist.user.profile.address
     return render(request, "wichtel/giftlist.html", {"giftlist": giftlist, "address": address})
